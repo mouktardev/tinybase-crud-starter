12bee2d3d4d6226c58bbf558c66b7d64aab71946
Artmade Studio <mouktarart@gmail.com>
2024-06-02 14:34:19 +0300

    remove autoComplete off

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -11,7 +11,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 				<input
 					type={type}
 					dir="auto"
-					autoComplete="off"
 					className={cn(
 						"flex h-10 w-full border border-accent bg-foreground/30 rounded-md px-3 py-1 text-sm text-primary file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-secondary focus-visible:outline-none focus-visible:ring-1",
 						className
